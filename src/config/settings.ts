1c771e34399f611f099f00e624f2654bbb8a9c56
Vinicius Bisognin Immich <vinicius@Viniciuss-MacBook-Pro.local>
2020-06-17 15:18:15 -0300

    move jwt setting to settings file

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,9 +1,15 @@
+// Change in production
 const BASE_URL = 'http://localhost:3333';
 const STATIC_BASE = '/static';
 const STATIC_URL = `${BASE_URL}${STATIC_BASE}/`;
+// JWT Confit
+const JWT_SECRET = '6ce200bf4bc7b8240352131bbcf0859b';
+const JWT_EXPIRES_IN = 86400; // Default to 24hours in seconds
 
 export {
   BASE_URL,
   STATIC_BASE,
   STATIC_URL,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
 };
--- src/utils/gerenerateWebToken.ts
+++ src/utils/gerenerateWebToken.ts
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken';
-import authConfig from '../config/auth.json';
+import { JWT_SECRET, JWT_EXPIRES_IN } from '../config/settings';
 
 const generateWebToken = (userInfo: Object) => {
-  const token = jwt.sign(userInfo, authConfig.secret, {
-    expiresIn: 86400,
+  const token = jwt.sign(userInfo, JWT_SECRET, {
+    expiresIn: JWT_EXPIRES_IN,
   });
 
   return token;
